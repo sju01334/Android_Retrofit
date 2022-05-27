40f69630d87ff06c0c0d1f73bf738a8813140ada
sju01334 <sju01334@gmail.com>
2022-05-27 12:24:09 +0900

    닉네임 변경 작업 완료

--- app/src/main/java/com/nepplus/android_retrofit/api/APIList.kt
+++ app/src/main/java/com/nepplus/android_retrofit/api/APIList.kt
@@ -7,7 +7,18 @@ import retrofit2.http.*
 
 interface APIList {
 
-    @FormUrlEncoded //파라미터중에 폼데이터로 담아야 하는 파라미터 있다면 작성
+//    user
+
+    @FormUrlEncoded
+    @PATCH("/user")
+    fun patchReqeustEditUser(
+        @Header("X-Http-Token") token : String,
+        @Field("nick_name") nickname : String?,
+        @Field("current_password") currentPw : String?,
+        @Field("new_password") newPw : String?
+    ) : Call<BasicResponse>
+
+            @FormUrlEncoded //파라미터중에 폼데이터로 담아야 하는 파라미터 있다면 작성
     @POST("/user")
     fun postRequestLogin(
         @Field("email") email: String,
@@ -21,7 +32,11 @@ interface APIList {
         @Field("nick_name") nick : String
     ) : Call<BasicResponse>
 
+//    main
     @GET("/main_info")
     fun getRequestMainInfo(@Header("X-Http-Token") token : String) : Call<BasicResponse>
+
+
+
 
 }
--- app/src/main/java/com/nepplus/android_retrofit/ui/setting/DetailProfileActivity.kt
+++ app/src/main/java/com/nepplus/android_retrofit/ui/setting/DetailProfileActivity.kt
@@ -2,14 +2,22 @@ package com.nepplus.android_retrofit.ui.setting
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
 import com.nepplus.android_retrofit.BaseActivity
 import com.nepplus.android_retrofit.R
 import com.nepplus.android_retrofit.databinding.ActivityDetailProfileBinding
+import com.nepplus.android_retrofit.dialog.CustomAlertDialog
+import com.nepplus.android_retrofit.models.BasicResponse
 import com.nepplus.android_retrofit.ui.main.LoginActivity
 import com.nepplus.android_retrofit.utils.ContextUtil
 import com.nepplus.android_retrofit.utils.GlobalData
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DetailProfileActivity : BaseActivity() {
 
@@ -31,10 +39,58 @@ class DetailProfileActivity : BaseActivity() {
             val myIntent = Intent(mContext, LoginActivity::class.java)
             myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK).addFlags((Intent.FLAG_ACTIVITY_NEW_TASK))
             startActivity(myIntent)
-
-
         }
 
+        binding.changeNickBtn.setOnClickListener {
+            val alert = CustomAlertDialog(mContext, this)
+            alert.myDialog(
+                "닉네임 변경",
+                "변경할 닉네임을 입력해주세요.",
+                false,
+                object : CustomAlertDialog.ButtonClickListener{
+                    override fun positiveBtnClicked() {
+                        val changedNick = alert.binding.contentEdt.text.toString()
+
+                        apiList.patchReqeustEditUser(
+                            ContextUtil.getLoginToken(mContext),
+                            changedNick,
+                            null,
+                            null
+                        ).enqueue(object : Callback<BasicResponse>{
+                            override fun onResponse(
+                                call: Call<BasicResponse>,
+                                response: Response<BasicResponse>
+                            ) {
+                                if(response.isSuccessful){
+                                    val br = response.body()!!
+                                    GlobalData.loginUser = br.data.user
+                                    binding.nicknameTxt.text = br.data.user.nick_name
+
+                                    Toast.makeText(mContext, "닉네임 변경이 완료되었습니다", Toast.LENGTH_SHORT).show()
+                                }
+                                else{
+                                  val jsonObj = JSONObject(response.errorBody()!!.string())
+                                  val code = jsonObj.getInt("code")  
+                                  val message = jsonObj.getString("message")
+                                  if(code == 400){
+                                      Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                                  }else{
+                                      Log.e("${TAG}_회원정보 수정 에러", message)
+                                  }
+                                }
+                            }
+                            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                            }
+                        })
+                    }
+
+                    override fun negativeBtnClicked() {
+                        TODO("Not yet implemented")
+                    }
+
+                }
+            )
+        }
     }
 
     override fun setValues() {
